747a88df8924ad96d5efae1b5b6033e8c59ccabb
l1rn <158823778+l1rn@users.noreply.github.com>
2024-11-11 00:15:30 +0500

    Add files via upload

---
+++ labs/1/MathApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MathLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Введите значение x, используя '.' (от -1 до 1, исключая 1): ");
+
+        if (double.TryParse(Console.ReadLine(), out double x) && x > -1 && x < 1)
+        {
+            double standardResult = Math.Log(1 - x); // Результат стандартной функции
+            double customResult = MathFunctions.LnOneMinusX(x); // Результат кастомной функции из DLL
+
+            Console.WriteLine($"Результат стандартной функции: {standardResult}");
+            Console.WriteLine($"Результат кастомной функции: {customResult}");
+        }
+        else 
+        {
+            Console.WriteLine("Некорректный ввод, возможно вы ввели число через ','. Значение x должно быть в диапазоне (-1, 1).");
+        }
+    }
+}
+ 
---
+++ labs/1/MathLibrary/MathFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class MathFunctions
+    {
+        public static double LnOneMinusX(double x)
+        {
+            double tolerance = 1e-9; // Точность
+            double term = x;         // Первый член ряда
+            double sum = -x;         // Начальное значение суммы
+            int n = 2;               // Индекс для рекурсии
+
+            while (Math.Abs(term) > tolerance)
+            {
+                term = Math.Pow(x, n) / n;
+                sum -= term; // Добавляем следующий член ряда
+                n++;
+            }
+
+            return sum;
+        }
+    }
+}
