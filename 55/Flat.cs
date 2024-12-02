e2a05cbcaaa6035515cb669de330dcdb35647be3
l1rn <158823778+l1rn@users.noreply.github.com>
2024-12-02 17:28:39 +0500

    Add files via upload

---
+++ 55/Flat.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab55
+{
+    internal class Flat
+    {
+        protected long personalAccount;
+        protected double squareFlat;
+        protected int quantityResidents;
+        protected double squarePrice;
+        protected double quantityResidentsPrice;
+        public Flat(long PersonalAccount, double SquareFlat,
+            int QuanrityResidents, double SquarePrice, double QuanityResidentsPrice)
+        {
+            this.personalAccount = PersonalAccount;
+            this.squareFlat = SquareFlat;
+            this.quantityResidents = QuanrityResidents;
+            this.squarePrice = SquarePrice;
+            this.quantityResidentsPrice = QuanityResidentsPrice;
+        }
+
+        public long GetPersonalAccount()
+        {
+            return personalAccount;
+        }
+        public virtual void PrintAllInfo()
+        {
+            Console.WriteLine($"Лицевой счёт: " + personalAccount);
+            Console.WriteLine($"Площадь квартиры: " + squareFlat + " кв.м.");
+            Console.WriteLine($"Количество проживающих: " + quantityResidents);
+            Console.WriteLine($"Оплата за площадь: " + squarePrice + " руб.");
+            Console.WriteLine($"Оплата за количество проживающих: " + quantityResidentsPrice + " руб.");
+        }
+
+        internal class GovernmentFlat : Flat
+        {
+            private double allRentPrice;
+            public GovernmentFlat(long PersonalAccount, double SquareFlat,
+            int QuanrityResidents, double SquarePrice, double QuanityResidentsPrice, double AllRentPrice) 
+                : base(PersonalAccount, SquareFlat,
+            QuanrityResidents, SquarePrice, QuanityResidentsPrice)
+            {
+                allRentPrice = AllRentPrice;
+            }
+            
+
+            public override void PrintAllInfo()
+            {
+                base.PrintAllInfo();
+                Console.WriteLine("Общая плата за найм: " + allRentPrice + " руб.");
+            }
+        }
+
+        internal class PrivatizeFlat : Flat
+        {
+            private double priceRepairFlat;
+            private double allPriceFlat;
+
+            public PrivatizeFlat(long PersonalAccount, double SquareFlat,
+            int QuanrityResidents, double SquarePrice, double QuanityResidentsPrice, double PriceRepairFlat)
+            : base (PersonalAccount, SquareFlat, QuanrityResidents, SquarePrice, QuanrityResidents)
+            {
+                this.priceRepairFlat = PriceRepairFlat;
+                allPriceFlat = priceRepairFlat + quantityResidentsPrice + squarePrice;
+            }
+
+            public override void PrintAllInfo()
+            {
+
+                base.PrintAllInfo();
+                Console.WriteLine("Плата за кап. ремонт: " + priceRepairFlat + " руб.");
+                Console.WriteLine("Общая плата за квартиру: " + allPriceFlat + " руб.");
+            }
+        }
+
+
+
+    }
+    
+}
---
+++ 55/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static lab55.Flat;
+
+namespace lab55
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Flat[] flats = new Flat[]
+            {
+                new Flat(1001, 40, 4, 40000, 10000),
+                new GovernmentFlat(1002, 50, 3, 45000, 10000, 60000),
+                new PrivatizeFlat(1003, 60, 2 , 60000, 10000, 30000)
+            };
+            int reply = -1;
+            while (reply != 3)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("|1 - вывод информации       |");
+                Console.WriteLine("|2 - поиск по лицевому счету|");
+                Console.WriteLine("|3 - выйти из программы     |");
+                Console.WriteLine("-----------------------------");
+                if (!int.TryParse(Console.ReadLine(), out reply))
+                {
+                    Console.WriteLine("Ошибка! Вы неправильно ввели.");
+                }
+                switch (reply)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Информация о всех квартирах:");
+                        foreach(var flat in flats)
+                        {
+                            flat.PrintAllInfo();
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.Write("Введите лицевой счет для поиска: ");
+                        if (!long.TryParse(Console.ReadLine(), out long searchAccount))
+                        {
+                            Console.WriteLine("Ошибка: введите корректный лицевой счет.");
+                            break;
+                        }
+
+                        Flat foundFlat = null;
+                        foreach (var flat in flats)
+                        {
+                            if (flat.GetPersonalAccount() == searchAccount)
+                            {
+                                foundFlat = flat;
+                                break;
+                            }
+                        }
+
+                        if (foundFlat != null)
+                        {
+                            Console.WriteLine("Найдена квартира:");
+                            foundFlat.PrintAllInfo();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Квартира с указанным лицевым счетом не найдена.");
+                        }
+                        break;
+                    case 3:
+                        break;
+                }
+                
+            }
+
+
+        }
+
+
+    }
+}
